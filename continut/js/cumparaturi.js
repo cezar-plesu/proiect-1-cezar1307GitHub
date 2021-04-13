5bf5a82d938789a072743f8378fcdc98816d9793
Plesu Cezar <cezar.plesu@student.tuiasi.ro>
2021-04-13 13:05:34 +0300

    Laborator 8

--- continut/js/cumparaturi.js
+++ continut/js/cumparaturi.js
@@ -1,27 +1,23 @@
-class Produs{
-    constructor(id,nume,cantitate){
+class Produs {
+    constructor(id, nume, cantitate) {
         this.id = id;
         this.nume = nume;
         this.cantitate = cantitate;
-        
+
     }
-    print(){
-        return this.id+' '+this.nume+' '+this.cantitate;
+    print() {
+        return this.id + ' ' + this.nume + ' ' + this.cantitate;
     }
 }
 
-function adauga(){
-    var nume=document.getElementById("produs").value;
-    var cant=document.getElementById("cantitate").value;
-    var id=localStorage.length+1;
-    produs=new Produs(id,nume,cant);
-    try{
-        localStorage.setItem(id,produs.print());
-        
-        alert("1");
-    }
-    catch{
-        alert("2");
-    }
+function adauga() {
+    var nume = document.getElementById("produs").value;
+    var cant = document.getElementById("cantitate").value;
+    var id = localStorage.length + 1;
+    produs = new Produs(id, nume, cant);
+
+    localStorage.setItem(id, produs.print());
+    alert("1");
+
 
 }
